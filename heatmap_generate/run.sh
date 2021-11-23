23e3c98f2ba9fc1599ba5f6227f6c5f6035f7aff
laochanlam <laochanlam@gmail.com>
2021-11-23 15:45:22 +0800

    update run script

--- heatmap_generate/run.sh
+++ heatmap_generate/run.sh
@@ -1,1 +1,8 @@
+# Imagenet
 python3 erase_experiment.py -a resnet50  -j 32 -e --pretrained --heatmap /data/imagenet18_all/data/imagenet/
+
+# CIFAR 10
+## TRAIN
+python3 erase_experiment.py -a resnet50  -j 32 --pretrained --heatmap /data/imagenet18_all/data/imagenet
+## EVAL
+python3 erase_experiment.py -a resnet50  -j 32 --pretrained -e --resume ./model_best.pth.tar --heatmap /data/imagenet18_all/data/imagenet
